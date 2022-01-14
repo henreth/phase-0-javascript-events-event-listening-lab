e89ad0eb926f43475858f52bfd45346e2f15fe79
Henry Miller <mail.henrym+git@gmail.com>
2022-01-14 09:22:57 -0500

    commit tree b39b1043dbea81e9e1bf7c8d013cd80b28a07f13

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-function addingEventListener() {
+// function addingEventListener() {
+//     const input = document.getElementById('input');
+
+//     function clickAlert() {
+//     alert('I was clicked!');
+//     }
+
+//     input.addEventListener('click', clickAlert);
+
+// }
+
+
+const addingEventListener = () => {
     const input = document.getElementById('input');
-
-    function clickAlert() {
-    alert('I was clicked!');
+    input.addEventListener('click', 
+        () => alert('I was clicked!')
+        )
     }
-
-    input.addEventListener('click', clickAlert);
-
-}
